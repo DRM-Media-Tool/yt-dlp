c218eda1dc0923cfcb78540e645faf89ef792d99
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-09-11 23:10:52 +0530

    Update mp4decrypt.py

--- yt-dlp-plugins/MP4Decrypt/yt_dlp_plugins/postprocessor/mp4decrypt.py
+++ yt-dlp-plugins/MP4Decrypt/yt_dlp_plugins/postprocessor/mp4decrypt.py
@@ -2,6 +2,7 @@ from yt_dlp.postprocessor.common import PostProcessor
 import os
 import subprocess
 
+
 class MP4DecryptPP(PostProcessor):
     def __init__(self, downloader=None, **kwargs):
         super().__init__(downloader)
@@ -9,7 +10,7 @@ class MP4DecryptPP(PostProcessor):
 
     def run(self, info):
         filepath = info.get('filepath')
-        
+
         if filepath:
             if 'decryption_key' in self._kwargs:
                 decryption_key = self._kwargs['decryption_key']
@@ -31,19 +32,19 @@ class MP4DecryptPP(PostProcessor):
             else:
                 self.to_screen("No decryption key or keyfile provided.")
                 return [], info
-        
+
         else:
             filepath = info.get('_filename')
             self.to_screen(f'Pre-processed "{filepath}" with {self._kwargs}')
-        
+
         return [], info
 
     def decrypt_single_key(self, filepath, decryption_key):
         try:
             output_file = f"{os.path.splitext(filepath)[0]}_decrypted{os.path.splitext(filepath)[1]}"
             cmd = ["mp4decrypt", "--key", decryption_key, filepath, output_file]
-            #USE FOR DEBUGGING PURPOSES
-            #self.to_screen(f'Executing command: {" ".join(cmd)}')
+            # USE FOR DEBUGGING PURPOSES
+            # self.to_screen(f'Executing command: {" ".join(cmd)}')
             subprocess.run(cmd, check=True)
             os.remove(filepath)
             os.rename(output_file, filepath)
@@ -55,20 +56,21 @@ class MP4DecryptPP(PostProcessor):
         try:
             with open(keyfile, 'r') as f:
                 keys = f.read().splitlines()
-            
+
             output_file = f"{os.path.splitext(filepath)[0]}_decrypted{os.path.splitext(filepath)[1]}"
             cmd = ["mp4decrypt"]
             for key in keys:
                 cmd.extend(["--key", key])
             cmd.extend([filepath, output_file])
-            #USE FOR DEBUGGING PURPOSES
-            #self.to_screen(f'Executing command: {" ".join(cmd)}')
+            # USE FOR DEBUGGING PURPOSES
+            # self.to_screen(f'Executing command: {" ".join(cmd)}')
             subprocess.run(cmd, check=True)
             os.remove(filepath)
             os.rename(output_file, filepath)
             return True
         except (FileNotFoundError, subprocess.CalledProcessError):
             return False
+
 
 def setup(downloader, **kwargs):
     downloader.add_post_processor(MP4DecryptPP(downloader, **kwargs))
